23ca4ac177b402f85f59b73e553d53dd347c348f
Sameer Ahmed <sameer.3for4@gmail.com>
2025-07-13 16:20:40 -0700

    added cors according to domain.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://mega-mall.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     credentials: true
